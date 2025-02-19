d938c3aa9cbe31fc9eae75103e9d34c6a0903fb5
Ednei Junior <ednei.junior26@gmail.com>
2025-02-19 15:29:23 -0300

    Removendo chamada duplicada

--- src/Http/Controllers/Api/ApiController.php
+++ src/Http/Controllers/Api/ApiController.php
@@ -52,28 +52,6 @@ class ApiController extends Controller
         return response()->json($colunas);
     }
 
-    public function listRREOAnexos()
-    {
-        $anexos = SiconfiRREO::select('anexo')->distinct()->get();
-        return response()->json($anexos);
-    }
-    
-    public function listRREOContasByAnexo($anexo)
-    {
-        $contas = SiconfiRREO::select('conta')->where('anexo', $anexo)->distinct()->get();
-        return response()->json($contas);
-    }
-
-    public function listRREOColunasByAnexoAndConta($anexo, $conta)
-    {
-        $colunas = SiconfiRREO::select('coluna')
-            ->where('anexo', $anexo)
-            ->where('conta', $conta)
-            ->distinct()
-            ->get();
-
-        return response()->json($colunas);
-    }
     public function listRGFAnexos()
     {
         $anexos = SiconfiRGF::select('anexo')->distinct()->get();
